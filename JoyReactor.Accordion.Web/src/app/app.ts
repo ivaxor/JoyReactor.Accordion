bc80042ff100a938bb2dfbe89f4684d88c13ad40
Illia Khovanskyi <saskiuhia@gmail.com>
2026-01-17 19:55:20 +0100

    Navbar

--- JoyReactor.Accordion.Web/src/app/app.ts
+++ JoyReactor.Accordion.Web/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from "./footer/footer";
+import { Navbar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Footer],
+  imports: [RouterOutlet, Footer, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
