5ee174c0bf6fc066d8f4b514a0a3eef7d325b18d
Illia Khovanskyi <saskiuhia@gmail.com>
2026-01-20 18:47:02 +0100

    RouterModule

--- JoyReactor.Accordion.Web/src/app/navbar/navbar-root/navbar-root.ts
+++ JoyReactor.Accordion.Web/src/app/navbar/navbar-root/navbar-root.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarHealthCheck } from "../navbar-healthcheck/navbar-healthcheck";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-root',
-  imports: [NavbarHealthCheck],
+  imports: [RouterModule, NavbarHealthCheck],
   templateUrl: './navbar-root.html',
   styleUrl: './navbar-root.scss',
 })
